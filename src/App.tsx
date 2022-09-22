972c06cd3abadcc1a7de4f612c458b89516edf68
mesequeteixeira <57111262+manoelmeseque@users.noreply.github.com>
2022-09-21 21:43:56 -0300

    Configurar font-family principal

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { GlobalStyle } from "./styles/global";
 export function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>Hello world</h1>
       <GlobalStyle />
     </div>
   );
